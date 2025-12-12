be7c6edd208393343a9d778bd3d605b50b677206
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2025-12-12 07:49:39 -0500

    chore: add shadcn, svelteflow and  dark-mode

---
+++ src/lib/components/flow-builder/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import {SvelteFlowProvider} from '@xyflow/svelte'
+	import Board from './board.svelte';
+</script>
+
+<SvelteFlowProvider>
+    <Board />
+</SvelteFlowProvider>
---
+++ src/lib/components/sidebar/index.svelte
@@ -0,0 +1,3 @@
+<div class="w-[300px] bg-white h-full border-border/50 border-r dark:bg-[#12161C]">
+    Sidebar content
+</div>
