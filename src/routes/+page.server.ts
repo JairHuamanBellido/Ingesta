0f1554c1551e0b0ea46fe89a35eff322c57316d1
Jair Huaman Bellido <huamanbellidoj@gmail.com>
2025-12-26 23:08:27 -0500

    feat: pipeline templates batch 1 (#49)

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,3 +1,4 @@
+import { PIPELINES_TEMPLATE } from '$core/pipeline/pipeline-template';
 import { createPipeline } from '../domain/use-cases/create-pipeline';
 import type { Actions } from './$types';
 import { redirect } from '@sveltejs/kit';
@@ -9,13 +10,15 @@ export const actions = {
 		const pipelineId = data.get('pipelineId') as string;
 		const description = data.get('description') as string;
 		const name = data.get('name') as string;
+		const pipelineKeyTemplate = data.get('key') as string;
+		const template = PIPELINES_TEMPLATE.find((template) => template.key === pipelineKeyTemplate);
 
 		const createPipelineResponse = await createPipeline({
 			edges: [],
 			nodes: [],
 			key: pipelineId,
 			description,
-			processors: [],
+			processors: template?.pipeline.processors || [],
 			name,
 			tests: []
 		});
