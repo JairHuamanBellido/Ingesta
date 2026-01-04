2911b8d0e265692951ba5c59c1b04170bf421003
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2026-01-04 12:22:27 -0500

    feat: delete simulation test

--- src/lib/components/flow-builder/index.svelte
+++ src/lib/components/flow-builder/index.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
+	import { setContext } from 'svelte';
 	import { SvelteFlowProvider } from '@xyflow/svelte';
 	import Board from './board.svelte';
 	import DnDProvider from './dnd-provider.svelte';
 	import type { IPipeline } from '../../../infrastructure/model/pipeline.model';
 
-	let { pipeline }: { pipeline: IPipeline } = $props();
+	let props: { pipeline: IPipeline } = $props();
+	setContext('pipeline', props.pipeline);
 </script>
 
 <SvelteFlowProvider>
 	<DnDProvider>
-		<Board {pipeline} />
+		<Board pipeline={props.pipeline} />
 	</DnDProvider>
 </SvelteFlowProvider>
